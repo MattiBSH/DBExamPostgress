d12e4d766c7cdc63de1f84068a00a113d23d96a2
MattiBSH <cph-mh829@cphbusiness.dk>
2022-05-08 00:28:53 +0200

    not everything but a start to tired to do everything

---
+++ src/test/java/com/example/exampostgress/ExamPostgressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exampostgress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamPostgressApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
