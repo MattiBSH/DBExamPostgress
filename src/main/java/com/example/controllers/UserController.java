a2d06799c9dc3e22e40596bb431788747396f69c
MattiBSH <cph-mh829@cphbusiness.dk>
2022-05-14 16:24:16 +0200

    enchanced

--- src/main/java/com/example/controllers/UserController.java
+++ src/main/java/com/example/controllers/UserController.java
@@ -1,10 +1,16 @@
 package com.example.controllers;
 
+import com.example.dto.ArrangementDTO;
 import com.example.dto.UserDTO;
+import com.example.models.Arrangement;
 import com.example.models.User;
 import com.example.payload.request.SignupRequest;
+import com.example.repositories.ArrangementRepository;
 import com.example.repositories.UserRepository;
+import com.example.security.services.ArrangementService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
@@ -36,7 +42,41 @@ public class UserController {
     @PreAuthorize("hasRole('MODERATOR') or hasRole('ADMIN')")
     public String deleteUser(@RequestParam(name = "email") String email) {
         userRepository.deleteUserByEmail(email);
+        return "Arrangement deleted";
+    }
+
+    @Autowired
+    ArrangementRepository arrangementRepository;
+    @Autowired
+    ArrangementService arrangementService;
+    @GetMapping("allArrangements")
+    public String allArrangements() {
+        List<Arrangement> arrangements=arrangementRepository.findAll();
+        List<ArrangementDTO> arrangementDTOs = new ArrayList<>();
+        System.out.println("hey");
+        for (int i = 0; i < arrangements.size(); i++) {
+            List<Long> ids = new ArrayList<>();
+            for (int j = 0; j < arrangements.get(i).getUsersParticipated().size(); j++) {
+                ids.add(arrangements.get(i).getUsersParticipated().get(j).getId());
+            }
+            arrangementDTOs.add(new ArrangementDTO(arrangements.get(i).getName(),arrangements.get(i).getType(),ids));
+        }
+        return gson.toJson(arrangementDTOs);
+    }
+
+    @DeleteMapping("/arrangement/delete")
+    @Transactional()
+    @PreAuthorize("hasRole('ADMIN')")
+    public String deleteArrangement(@RequestParam(name = "id") Long id) {
+        arrangementRepository.deleteArrangementById(id);
         return "User deleted";
+    }
+
+    @PostMapping("/arrangement")
+    @PreAuthorize("hasRole('ADMIN')")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<String> createArrangement(@RequestBody ArrangementDTO arrangementDTO){
+        return new ResponseEntity<>(arrangementService.createArrangement(arrangementDTO), HttpStatus.CREATED);
     }
 }
 
